e5c31c9b8a1ab9d2111aad70a2583d66bdedbad3
Daria Pritchina <dasha.bayanova@gmail.com>
2025-12-01 14:34:31 +0300

    WhenSetKey_ForExistingKey_ValueWillBeUpdated

--- Tests/WhenSetKey.cs
+++ Tests/WhenSetKey.cs
@@ -18,4 +18,20 @@ public class WhenSetKey
         // Assert
         Assert.That(store.Get("anyNewKey"), Is.EqualTo(value));
     }
+
+    [Test]
+    public void ForExistingKey_ValueWillBeUpdated()
+    {
+        // Arrange
+        const string key = "anyKey";
+        var store = Create.Store()
+            .WithKeyValue(key, "oldValue"u8.ToArray())
+            .Please();
+        
+        // Act
+        store.Set(key, "newValue"u8.ToArray());
+        
+        // Assert
+        Assert.That(store.Get(key), Is.EqualTo("newValue"u8.ToArray()));
+    }
 }
