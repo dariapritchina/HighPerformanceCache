d0d0868d6356493a4869adcff91ee9a140ced51e
Daria Pritchina <dasha.bayanova@gmail.com>
2025-12-03 17:13:02 +0300

    refactor CommandParser:Parse

--- Cache.Domain/Impl/CommandParser.cs
+++ Cache.Domain/Impl/CommandParser.cs
@@ -5,11 +5,12 @@ namespace Cache.Domain.Impl;
 public class CommandParser : ICommandParser
 {
     private const char SEPARATOR = ' ';
+    private const int NOT_FOUND_INDEX = -1;
     public static CommandInfo Parse(ReadOnlySpan<char> input)
     {
         var command = SliceNextPart(input);
         var key = SliceNextPart(input, command.Length + 1);
-        var value = input.Slice(command.Length + 1 + key.Length + 1);
+        var value = SliceNextPart(input, command.Length + 1 + key.Length + 1);
 
         return new CommandInfo(
             command: command,
@@ -19,10 +20,11 @@ public class CommandParser : ICommandParser
 
     private static ReadOnlySpan<char> SliceNextPart(ReadOnlySpan<char> input, int startFrom = 0)
     {
-        input = input.Slice(startFrom);
+        input = input[startFrom..];
         var firstIndexOfSeparator = input.IndexOf(SEPARATOR);
 
-        var part = input.Slice(0, firstIndexOfSeparator);
-        return part;
+        return firstIndexOfSeparator == NOT_FOUND_INDEX 
+            ? input 
+            : input[..firstIndexOfSeparator];
     }
 }
