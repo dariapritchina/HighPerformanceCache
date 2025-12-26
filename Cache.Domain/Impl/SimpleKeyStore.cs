1ab6a8c067246a38d73a9827ac76099338881c4b
Daria Pritchina <dasha.bayanova@gmail.com>
2025-12-26 14:45:18 +0300

    One more concurrent tests

--- Cache.Domain/Impl/SimpleKeyStore.cs
+++ Cache.Domain/Impl/SimpleKeyStore.cs
@@ -16,19 +16,25 @@ public class SimpleKeyStore : IKeyStore, IDisposable
     {
         CheckKeyIsNotNullOrEmpty(key);
 
-        if (!ContainsKey(key))
+        try
         {
-            DoAddKeyValue(key, value);
+            _lock.EnterWriteLock();
+            
+            if (!_keyValues.ContainsKey(key))
+                _keyValues.Add(key, value);
+            else
+                _keyValues[key] = value;
         }
-        else
+        finally
         {
-            DoUpdateKeyValue(key, value);
+            _lock.ExitWriteLock();
         }
     }
 
     public byte[]? Get(string key)
     {
         CheckKeyIsNotNullOrEmpty(key);
+        
         try
         { 
             _lock.EnterReadLock();
@@ -42,56 +48,17 @@ public class SimpleKeyStore : IKeyStore, IDisposable
 
     public void Delete(string key)
     {
-        if (!ContainsKey(key))
-            throw new ArgumentException($"Key \'{key}\' not found");
-
         try
         {
-            _lock.EnterWriteLock(); 
+            _lock.EnterWriteLock();
+            
+            if (!_keyValues.ContainsKey(key))
+                throw new ArgumentException($"Key \'{key}\' not found");
             _keyValues.Remove(key);
         }
         finally
         {
             _lock.ExitWriteLock();
-        }
-    }
-
-    private void DoAddKeyValue(string key, byte[] value)
-    {
-        try
-        {
-            _lock.EnterWriteLock();
-            _keyValues.Add(key, value);
-        }
-        finally
-        {
-            _lock.ExitWriteLock();
-        }
-    }
-    
-    private void DoUpdateKeyValue(string key, byte[] value)
-    {
-        try
-        {
-            _lock.EnterWriteLock();
-            _keyValues[key] = value;
-        }
-        finally
-        {
-            _lock.ExitWriteLock();
-        }
-    }
-
-    private bool ContainsKey(string key)
-    {
-        try
-        {
-            _lock.EnterReadLock();
-            return _keyValues.ContainsKey(key);
-        }
-        finally
-        {
-            _lock.ExitReadLock();;
         }
     }
     
--- UnitTests/SimpleKeyStoreTests/WhenUseConcurrentAccess.cs
+++ UnitTests/SimpleKeyStoreTests/WhenUseConcurrentAccess.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Text;
 using UnitTests.DSL;
 
 namespace UnitTests;
@@ -71,5 +72,93 @@ public class WhenUseConcurrentAccess
         
         // Assert
         Assert.All(readValues.ToArray(), v => Assert.Equal(v, storedValue));
+    }
+    
+    [Theory]
+    [InlineData(1000, 30)]
+    public async Task WithMultipleWriters_ShouldHasNoExceptions(int writersCount, int operationsPerTask)
+    {
+        // Arrange
+        var storedKey = "anyKey";
+        using var store = Create.Store().Please();
+        var random = new Random();
+        var exceptions = new ConcurrentBag<Exception>();
+        
+        // Act
+        var writerTasks = Enumerable.Range(0, writersCount)
+            .Select(iWriter => Task.Run(async () =>
+            {
+                try
+                {
+                    for (var i = 0; i < operationsPerTask; i++)
+                    {
+                        store.Set(storedKey, Encoding.UTF8.GetBytes($"any_value_{i}"));
+                        await Task.Delay(random.Next(0, 20));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }))
+            .ToArray();
+        await Task.WhenAll(writerTasks);
+        
+        // Assert
+        Assert.Empty(exceptions);
+    }
+    
+    [Theory]
+    [InlineData(100, 100, 30)]
+    public async Task WithMultipleReadersAndWriters_ShouldHasNoExceptions(int readersCount, int writersCount, int operationsPerTask)
+    {
+        // Arrange
+        var storedKey = "anyKey";
+        var storedValue = "anyValue"u8.ToArray();
+        using var store = Create.Store()
+            .WithKeyValue(storedKey, storedValue)
+            .Please();
+        var random = new Random();
+        var exceptions = new ConcurrentBag<Exception>();
+        
+        // Act
+        var readerTasks = Enumerable.Range(0, readersCount)
+            .Select(iReader => Task.Run(async () =>
+            {
+                try
+                {
+                    for (var i = 0; i < operationsPerTask; i++)
+                    {
+                        var value = store.Get(storedKey);
+                        await Task.Delay(random.Next(0, 20));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }))
+            .ToArray();
+        var writerTasks = Enumerable.Range(0, writersCount)
+            .Select(iWriter => Task.Run(async () =>
+            {
+                try
+                {
+                    for (var i = 0; i < operationsPerTask; i++)
+                    {
+                        store.Set(storedKey, storedValue);
+                        await Task.Delay(random.Next(0, 20));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }))
+            .ToArray();
+        await Task.WhenAll(readerTasks.Concat(writerTasks));
+        
+        // Assert
+        Assert.Empty(exceptions);
     }
 }
